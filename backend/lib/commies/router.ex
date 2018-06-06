e516ec0fe0bcfd5e1f624cb04f4c4826ad4b80d8
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-06-06 11:47:11 +0200

    Support Github authenticate endpoints

--- backend/lib/commies/router.ex
+++ backend/lib/commies/router.ex
@@ -2,8 +2,10 @@ defmodule Commies.Router do
   use Plug.Router
 
   alias Commies.{
+    Auth,
+    Comment,
     Repo,
-    Comment
+    User
   }
 
   plug(
@@ -53,6 +55,60 @@ defmodule Commies.Router do
 
         send_json_resp(conn, 200, body)
     end
+  end
+
+  get "/login/github" do
+    redirect_url = Auth.Github.oauth_url("user:email", "http://localhost:8000/auth/github")
+
+    conn
+    |> put_resp_header("location", redirect_url)
+    |> send_resp(302, [])
+  end
+
+  get "/auth/github" do
+    code = Map.fetch!(conn.query_params, "code")
+
+    with {:ok, access_token} <- Auth.Github.exchange_access_token(code),
+         {:ok, user} <- Auth.Github.get_user(access_token),
+         {:ok, email} <- Auth.Github.get_user_email(access_token) do
+      params = %{
+        name: user.name,
+        email: email,
+        auth_provider: "github",
+        auth_user_id: user.id
+      }
+
+      case upsert_user(params) do
+        {:ok, user} ->
+          send_json_resp(conn, 200, user)
+
+        {:error, changeset} ->
+          body = %{
+            errors: format_changeset_errors(changeset)
+          }
+
+          send_json_resp(conn, 400, body)
+      end
+    else
+      _ ->
+        body = %{
+          errors: ["unable to authenticate user"]
+        }
+
+        send_json_resp(conn, 400, body)
+    end
+  end
+
+  defp upsert_user(params) do
+    changeset = User.upsert_changeset(params)
+
+    insert_options = [
+      returning: [:id],
+      on_conflict: :replace_all,
+      conflict_target: [:auth_provider, :auth_user_id]
+    ]
+
+    Repo.insert(changeset, insert_options)
   end
 
   defp send_json_resp(conn, status, body) do
