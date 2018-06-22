6bb59749ef5711bf46edfa5c36a49c9c140a988a
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-06-22 10:54:41 +0200

    Log all metadata

--- backend/config/config.exs
+++ backend/config/config.exs
@@ -4,4 +4,8 @@ config :commies, ecto_repos: [Commies.Repo]
 
 config :commies, Commies.Repo, adapter: Ecto.Adapters.Postgres
 
+config :logger, :console,
+  format: "\n$time $metadata[$level] $message\n",
+  metadata: :all
+
 import_config "env/#{Mix.env()}.exs"
