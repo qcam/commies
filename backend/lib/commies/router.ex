8a659a239b0af89216c7e0e422d9c64724cda492
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-06-22 11:14:32 +0200

    Split Comment Router

--- backend/lib/commies/router.ex
+++ backend/lib/commies/router.ex
@@ -1,11 +1,8 @@
 defmodule Commies.Router do
   use Plug.Router
 
-  import Ecto.Query
-
   alias Commies.{
     Auth,
-    Comment,
     Repo,
     RouteHelper,
     User
@@ -27,92 +24,7 @@ defmodule Commies.Router do
     send_resp(conn, 200, [])
   end
 
-  get "/links/:link_id/comments" do
-    comments =
-      Comment
-      |> where(link_id: ^link_id)
-      |> Repo.all()
-
-    body = %{
-      comments: comments
-    }
-
-    send_json_resp(conn, 200, body)
-  end
-
-  post "/links/:link_id/comments" do
-    if user = conn.assigns[:authenticated_user] do
-      changeset =
-        conn.body_params
-        |> Map.put("link_id", link_id)
-        |> Map.put("user_id", user.id)
-        |> Comment.create_changeset()
-
-      case Repo.insert(changeset) do
-        {:ok, comment} ->
-          send_json_resp(conn, 200, comment)
-
-        {:error, changeset} ->
-          body = %{
-            errors: format_changeset_errors(changeset)
-          }
-
-          send_json_resp(conn, 400, body)
-      end
-    else
-      send_json_resp(conn, 401, [])
-    end
-  end
-
-  put "/links/:link_id/comments/:comment_id" do
-    if user = conn.assigns[:authenticated_user] do
-      comment =
-        Comment
-        |> where(id: ^comment_id)
-        |> where(user_id: ^user.id)
-        |> Repo.one()
-
-      if comment do
-        changeset = Comment.update_changeset(comment, conn.body_params)
-
-        case Repo.update(changeset) do
-          {:ok, comment} ->
-            send_json_resp(conn, 200, comment)
-
-          {:error, changeset} ->
-            body = %{
-              errors: format_changeset_errors(changeset)
-            }
-
-            send_json_resp(conn, 400, body)
-        end
-      else
-        send_json_resp(conn, 404, %{errors: ["not found"]})
-      end
-    else
-      send_json_resp(conn, 401, [])
-    end
-  end
-
-  delete "/links/:link_id/comments/:comment_id" do
-    if user = conn.assigns[:authenticated_user] do
-      delete_result =
-        Comment
-        |> where(id: ^comment_id)
-        |> where(user_id: ^user.id)
-        |> Repo.delete_all()
-
-      case delete_result do
-        {1, nil} ->
-          send_resp(conn, 204, [])
-
-        {0, _} ->
-          send_json_resp(conn, 404, %{errors: ["not found"]})
-      end
-    else
-      send_json_resp(conn, 401, [])
-    end
-  end
+  forward("/links/:link_id/comments", to: Commies.Router.Comment)
 
   get "/login/github" do
     callback_url = RouteHelper.append_base("/auth/github")
@@ -180,13 +92,13 @@ defmodule Commies.Router do
     Repo.insert(changeset, insert_options)
   end
 
-  defp send_json_resp(conn, status, body) do
+  def send_json_resp(conn, status, body) do
     conn
     |> put_resp_header("content-type", "application/json")
     |> send_resp(status, Jason.encode!(body))
   end
 
-  defp format_changeset_errors(changeset) do
+  def format_changeset_errors(changeset) do
     Enum.map(changeset.errors, fn {field_name, {error, _}} ->
       "#{field_name} #{error}"
     end)
---
+++ backend/lib/commies/router/comment.ex
@@ -0,0 +1,106 @@
+defmodule Commies.Router.Comment do
+  use Plug.Router
+
+  import Ecto.Query
+
+  alias Commies.{
+    Comment,
+    Repo,
+    Router
+  }
+
+  plug(Plug.Logger)
+  plug(:match)
+  plug(:dispatch)
+
+  get "/" do
+    link_id = conn.params["link_id"]
+
+    comments =
+      Comment
+      |> where(link_id: ^link_id)
+      |> Repo.all()
+
+    body = %{
+      comments: comments
+    }
+
+    Router.send_json_resp(conn, 200, body)
+  end
+
+  post "/" do
+    if user = conn.assigns[:authenticated_user] do
+      link_id = conn.params["link_id"]
+
+      changeset =
+        conn.body_params
+        |> Map.put("link_id", link_id)
+        |> Map.put("user_id", user.id)
+        |> Comment.create_changeset()
+
+      case Repo.insert(changeset) do
+        {:ok, comment} ->
+          Router.send_json_resp(conn, 200, comment)
+
+        {:error, changeset} ->
+          body = %{
+            errors: Router.format_changeset_errors(changeset)
+          }
+
+          Router.send_json_resp(conn, 400, body)
+      end
+    else
+      Router.send_json_resp(conn, 401, [])
+    end
+  end
+
+  put "/:comment_id" do
+    if user = conn.assigns[:authenticated_user] do
+      comment =
+        Comment
+        |> where(id: ^comment_id)
+        |> where(user_id: ^user.id)
+        |> Repo.one()
+
+      if comment do
+        changeset = Comment.update_changeset(comment, conn.body_params)
+
+        case Repo.update(changeset) do
+          {:ok, comment} ->
+            Router.send_json_resp(conn, 200, comment)
+
+          {:error, changeset} ->
+            body = %{
+              errors: Router.format_changeset_errors(changeset)
+            }
+
+            Router.send_json_resp(conn, 400, body)
+        end
+      else
+        Router.send_json_resp(conn, 404, %{errors: ["not found"]})
+      end
+    else
+      Router.send_json_resp(conn, 401, [])
+    end
+  end
+
+  delete "/:comment_id" do
+    if user = conn.assigns[:authenticated_user] do
+      delete_result =
+        Comment
+        |> where(id: ^comment_id)
+        |> where(user_id: ^user.id)
+        |> Repo.delete_all()
+
+      case delete_result do
+        {1, nil} ->
+          send_resp(conn, 204, [])
+
+        {0, _} ->
+          Router.send_json_resp(conn, 404, %{errors: ["not found"]})
+      end
+    else
+      Router.send_json_resp(conn, 401, [])
+    end
+  end
+end
