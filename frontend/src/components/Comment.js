80c4eaa36774cbc0af4289e01bd02007b4bb0a97
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-10-28 13:52:32 +0100

    Add scss support

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./Comment.css";
+import "./Comment.scss";
 
 const Comment = (comment) => {
   return (
     <li className="comment">
-      <div className="user">{comment.user.name}</div>
-      <div className="content">{comment.content}</div>
+      <div className="comment__user">{comment.user.name}</div>
+      <div className="comment__content">{comment.content}</div>
     </li>
   );
 }
