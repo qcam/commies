e9fea8d69e043e0e8924e662f9bdb11ad80e3a4a
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-10-30 21:08:54 +0100

    Change endpoint URL

--- backend/config/env/prod.exs
+++ backend/config/env/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :commies, Commies.Auth.Github,
   http_client: Commies.HTTP.StandardClient
 
-config :commies, Commies.RouteHelper, base: "http://commies-sg.hqc.me"
+config :commies, Commies.RouteHelper, base: "http://commies.hqc.me/api"
 
 config :commies, :frontend, endpoint: "http://commies.hqc.me"
 
